9d91c76e50ae42efdc7d6586275f6a8680c06312
nguyenthanhluong <92586539+KYRim123@users.noreply.github.com>
2023-03-22 10:47:16 +0700

    up

--- client/src/redux/action.js
+++ client/src/redux/action.js
@@ -1,9 +1,9 @@
 import {createActions} from 'redux-actions'
-
+//fnc get type of actions
 export const getTypeActions = (reduxActions) => {
     return reduxActions().type
 }
-
+//action for page products
 export const getProducts = createActions({
     getProductsRequest: undefined,
     getProductsSuccess: (payload => payload),
